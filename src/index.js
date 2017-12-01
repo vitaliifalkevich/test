541fb138d29fc89fd03a06dcf6ba124e4ac3d3fe
Vitaliy Falkevich <vitaliifalkevich@gmail.com>
2017-12-01 11:44:42 +0200

    Create base structure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var $ = require('jquery');
+var str = require('./constants');
+var User = function() {
+    $('body').html(str);
+};
+
+var app = new User();
+
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+var webpack = require('webpack');
+
+module.exports= {
+    entry: './src/index.js',
+    output: {
+        filename: 'public/bundle.js'
+    }
+};
